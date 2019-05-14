52e758efbc39b4ccff6a6ec12f9924efa7e6125b
Kishor Pandey <kishor.jpandey@gmail.com>
2019-05-14 11:12:38 -0700

    added header

--- src/components/header.js
+++ src/components/header.js
@@ -4,8 +4,17 @@ import { Link } from 'react-router-dom';
 const Header = props => {
   return (
     <header>
-      <nav>
-      </nav>
+      <ul>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
     </header>
   );
 };
