488bcca410bd796ce8e2984f2e934bbd708c2c2b
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-15 01:21:34 -0700

    make minor change to error-message

--- src/components/error-message.js
+++ src/components/error-message.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorMessage = ({message}) => {
   return (
     <div className="error-message">
-        <h5>{message}</h5>
+      <h5>{message}</h5>
     </div>
   )
 }
