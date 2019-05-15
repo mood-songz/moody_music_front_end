67a97998847b0ea5f075a10bd299552f8d219d79
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2019-05-15 02:58:42 -0700

    add link to handle local backend or delpoyed backend. Add error component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-
+//gets a file upload
 class Image extends React.Component {
   constructor(props){
     super(props);
@@ -11,26 +10,33 @@ class Image extends React.Component {
     }
   }
 
-
 fileSelectedHandler= async e=>{
-e.preventDefault();
-await this.setState({
-  selectedFile: URL.createObjectURL(e.target.files[0]),
-  file:e.target.files[0]
-})
-
+  e.preventDefault();
+  await this.setState({
+    selectedFile: URL.createObjectURL(e.target.files[0]),
+    file:e.target.files[0]
+  })
 }
-render(){                                                
-    return(
-      <div className="App">
-      <form action="http://localhost:8000/upload" encType="multipart/form-data" method="POST"> 
-      <input type="file" name="theFile" onChange={this.fileSelectedHandler} accept="image/*"/>
-      <input type="submit"  value="Upload Photo" />
+
+updateErrorMessage = errorMessage => {
+  this.props.handleErrorMessage(errorMessage);
+}
+
+render(){
+  let backendUrl = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:8080' : '';
+  let backendUploadUrl = `${backendUrl}/upload`;    
+                                          
+  return(
+    <div className="file-uploader">
+      <h3>Upload an image below</h3>
+      <form action={backendUploadUrl} encType="multipart/form-data" method="POST"> 
+        <input type="file" name="theFile" onChange={this.fileSelectedHandler} accept="image/*"/>
+        <input type="submit"  value="Upload Photo" />
       </form>
       <img src={this.state.selectedFile} alt="" />
       {/* <button onClick={this.fileUploadHandler}>Upload My Image</button> */}
-      </div>
-    )
+    </div>
+  )
   } 
 }
 
